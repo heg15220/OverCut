5217efea0308a9b84c424dc1beaaf355284695ce
Hugo Espasandin Garcia <h.espasandin@udc.es>
2024-03-12 12:09:22 +0100

    Implemented store/root directory

---
+++ frontend/store/rootReducer.js
@@ -0,0 +1,10 @@
+import ReactDOM from "react-dom";
+import App from './src/App';
+
+const container = document.getElementById('root');
+
+// Crear un root.
+const root = ReactDOM.createRoot(container);
+
+// Renderizar inicialmente
+root.render(<App />);
