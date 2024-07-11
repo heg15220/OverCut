bef0938433e8da5491c42e6e536690021173aed1
Hugo Espasandin Garcia <h.espasandin@udc.es>
2024-07-12 00:11:57 +0200

    First Update for Calendar view

--- frontend/src/modules/events/components/MyCalendar.js
+++ frontend/src/modules/events/components/MyCalendar.js
@@ -1,36 +1,35 @@
-import React, { useState } from 'react';
-import { Calendar, momentLocalizer } from 'react-big-calendar';
+import React, { useState, useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import * as selectors from '../../events/selectors'; // Asegúrate de tener este hook creado
+import * as actions from '../../events/actions';
 import moment from 'moment';
-import './MyCalendar.css';
-import CreateEventForm from './CreateEventForm';
-const localizer = momentLocalizer(moment);
+import MyCustomCalendar from "./MyCustomCalendar";
 
 const MyCalendar = () => {
-    const [showModal, setShowModal] = useState(false);
-    const [selectedDate, setSelectedDate] = useState(null);
+    const dispatch = useDispatch();
+    const events = useSelector(selectors.getEvents); // Usa el selector de Redux para obtener los eventos
+    const [items, setItems] = useState([]);
 
+    useEffect(() => {
+        dispatch(actions.getEvents(0, () => {})); // Carga los eventos cuando el componente monta
+    }, [dispatch]);
 
-    const handleDateSelect = (date) => {
-        setSelectedDate(date);
-        setShowModal(true);
-    };
+    useEffect(() => {
+        if (events) {
+            const formattedEvents = events.items.map(event => ({
+                title: event.name,
+                startDate: moment(event.date).toDate(),
+                endDate: moment(event.date).add(1, 'hours').toDate(), // Asumiendo que cada evento dura 1 hora
+                description: event.description,
+                location: event.location,
+                imageUrl: event.imageUrl,
+            }));
+            setItems(formattedEvents);
+        }
+    }, [events]);
 
     return (
-        <div style={{ width: '80%', margin: '0 auto', overflowX: 'auto' }}>
-            <h1>Calendario</h1>
-            <div style={{ width: '100%', height: '500px', border: '1px solid #ccc' }}>
-                <Calendar
-                    localizer={localizer}
-                    defaultDate={new Date()}
-                    defaultView="week"
-                    selectable
-                    onSelectEvent={() => {}}
-                    onSelectSlot={handleDateSelect}
-                >
-                </Calendar>
-            </div>
-            {showModal && <CreateEventForm showModal={showModal} setShowModal={setShowModal} selectedDate={selectedDate}/>}
-        </div>
+        <MyCustomCalendar events={items}> </MyCustomCalendar>
     );
 };
 
