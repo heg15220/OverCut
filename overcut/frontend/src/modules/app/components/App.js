b16864efe042480c18ee351d68e6809f7a8d4e98
Hugo Espasandin Garcia <h.espasandin@udc.es>
2025-07-12 12:54:21 +0200

    Implemented MobileHeader in App

--- overcut/frontend/src/modules/app/components/App.js
+++ overcut/frontend/src/modules/app/components/App.js
@@ -1,26 +1,40 @@
-import React from "react";
-
+// App.jsx
+import React, { useEffect, useState } from "react";
 import { HashRouter as Router } from "react-router-dom";
-
 import { Helmet } from 'react-helmet';
 
 import Body from "./Body";
 import Footer from "./Footer";
 import Header from "./Header";
+import MobileHeader from "./MobileHeader"; // tu nuevo header móvil
+
+// Hook para ancho de ventana
+const useWindowWidth = () => {
+  const [width, setWidth] = useState(window.innerWidth);
+  useEffect(() => {
+    const handleResize = () => setWidth(window.innerWidth);
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+  return width;
+};
 
 const App = () => {
-    return (
-        <Router>
-            <Helmet>
-                <meta charset="UTF-8" />
-            </Helmet>
-            <div className="App">
-                <Header />
-                <Body />
-                <Footer />
-            </div>
-        </Router>
-    );
+  const windowWidth = useWindowWidth();
+  const isMobile = windowWidth <= 768;
+
+  return (
+    <Router>
+      <Helmet>
+        <meta charSet="UTF-8" />
+      </Helmet>
+      <div className="App">
+        {isMobile ? <MobileHeader /> : <Header />}
+        <Body />
+        <Footer />
+      </div>
+    </Router>
+  );
 };
 
 export default App;
---
+++ overcut/frontend/src/modules/app/components/MobileHeader.js
@@ -0,0 +1,132 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { FormattedMessage } from "react-intl";
+import { useSelector } from "react-redux";
+import users from '../../users';
+import { Lightbulb, Puzzle, GraphUp, Book, Trophy, List, X, PersonCircle, BoxArrowRight, PencilSquare } from "react-bootstrap-icons";
+import "./MobileHeader.css";
+import image from './Resources/LogoOverCut.png';
+
+const MobileHeader = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const isLogged = useSelector(users.selectors.isLoggedIn);
+  const user = useSelector(users.selectors.getUser);
+
+  const handleToggleMenu = () => setMenuOpen(!menuOpen);
+  const handleCloseMenu = () => setMenuOpen(false);
+
+  return (
+    <>
+      <header className="mobile-header">
+        <button className="hamburger-button" onClick={handleToggleMenu} aria-label="Open menu">
+          <List size={28} color="#fff" />
+        </button>
+        <Link to="/" className="mobile-header-logo" onClick={handleCloseMenu}>
+          <img src={image} alt="OverCut Logo" />
+        </Link>
+      </header>
+
+      {menuOpen && (
+        <div className="mobile-header-menu">
+          {/* Botón de cerrar */}
+          <button className="close-button" onClick={handleCloseMenu} aria-label="Close menu">
+            <X size={32} color="#fff" />
+          </button>
+
+          <nav>
+            <ul>
+              <li>
+                <Link to="/category/2" onClick={handleCloseMenu}>
+                  <FormattedMessage id="project.app.Header.quiz" /> <Lightbulb size={16} />
+                </Link>
+              </li>
+              <li>
+                <Link to="/minigames" onClick={handleCloseMenu}>
+                  <FormattedMessage id="project.app.Header.minigames" /> <Puzzle size={16} />
+                </Link>
+              </li>
+              <li>
+                <a href="http://localhost:8083/" onClick={handleCloseMenu}>
+                  <FormattedMessage id="project.app.Header.f1hub" /> <GraphUp size={16} />
+                </a>
+              </li>
+              <li>
+                <Link to="/about" onClick={handleCloseMenu}>
+                  <FormattedMessage id="project.app.Header.about" /> <Book size={16} />
+                </Link>
+              </li>
+              {isLogged && (
+                <li>
+                  <Link to="/users/ranking" onClick={handleCloseMenu}>
+                    <FormattedMessage id="project.app.Header.ranking" /> <Trophy size={16} />
+                  </Link>
+                </li>
+              )}
+            </ul>
+
+            {isLogged && (
+              <>
+                <hr className="mobile-menu-divider" />
+                <ul className="user-section">
+                  <li className="user-greeting">
+                    <PersonCircle size={20} /> {user.userName}
+                  </li>
+                  <li>
+                    <Link to="/users/update-profile" onClick={handleCloseMenu}>
+                      <FormattedMessage id="project.users.UpdateProfile.title" /> <PersonCircle size={16} />
+                    </Link>
+                  </li>
+                  <li>
+                    <Link to="/users/change-password" onClick={handleCloseMenu}>
+                      <FormattedMessage id="project.users.ChangePassword.title" /> <PencilSquare size={16} />
+                    </Link>
+                  </li>
+                  {user.journalist && (
+                    <>
+                      <li>
+                        <Link to="/create-post" onClick={handleCloseMenu}>
+                          <FormattedMessage id="project.users.CreatePost.title" /> <PencilSquare size={16} />
+                        </Link>
+                      </li>
+                      <li>
+                        <Link to="/post/my" onClick={handleCloseMenu}>
+                          <FormattedMessage id="project.users.MyPosts.title" /> <PencilSquare size={16} />
+                        </Link>
+                      </li>
+                    </>
+                  )}
+                  <li>
+                    <Link to="/users/logout" onClick={handleCloseMenu}>
+                      <FormattedMessage id="project.app.Header.logout" /> <BoxArrowRight size={16} />
+                    </Link>
+                  </li>
+                </ul>
+              </>
+            )}
+
+            {!isLogged && (
+              <>
+                <hr className="mobile-menu-divider" />
+                <ul>
+                  <li>
+                    <Link to="/users/login" onClick={handleCloseMenu}>
+                      <FormattedMessage id="project.app.Header.login" />
+                    </Link>
+                  </li>
+                  <li>
+                    <Link to="/users/signUp" onClick={handleCloseMenu}>
+                      <FormattedMessage id="project.users.SignUp.title" />
+                    </Link>
+                  </li>
+                </ul>
+              </>
+            )}
+          </nav>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default MobileHeader;
